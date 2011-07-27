d64dce5f449450567dd914f3b2356b4896df9dc9
dkapla02 <dkapla02@30e6160e-469f-4000-86ef-8ea788e09201>
2011-07-27 17:59:42 +0000

    svn merge -r 95698:95706 /tdr/bin/taper-trunk
    per DCA-678
    
    
    git-svn-id: svn://vm36.atech.tufts.edu/TAPER/production@95707 30e6160e-469f-4000-86ef-8ea788e09201

--- TAPER/lib/TAPER/Logic/AuditStamp.pm
+++ TAPER/lib/TAPER/Logic/AuditStamp.pm
@@ -1,3 +1,36 @@
+#
+# COPYRIGHT:
+#
+# Copyright 2010, 2011 Tufts University.
+#
+#
+# FUNDING:
+#
+# The development of the TAPER software suite was funded by the
+# National Historic Publications and Records Commission (NHPRC).
+# Grant number RE10005-08.
+#
+#
+# LICENSE:
+#
+# This file is part of the TAPER software suite.
+#
+# The TAPER software suite is free software: you can redistribute
+# it and/or modify it under the terms of the GNU Affero General
+# Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later
+# version.
+#
+# The TAPER software suite is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License along with the TAPER software suite.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+
 package TAPER::Logic::AuditStamp;
 
 use warnings;
